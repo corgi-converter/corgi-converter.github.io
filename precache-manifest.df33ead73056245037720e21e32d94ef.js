ea15cc36dea72ad46af9d2c4df7f7010657897e0
Amelia Rowe <ameliakathleenrowe@gmail.com>
2020-07-27 22:33:55 +1000

    Updates

---
+++ precache-manifest.df33ead73056245037720e21e32d94ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1b2b959c14095e5659f74ac25d2637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6784a7cd17985a4c192",
+    "url": "/static/js/2.113b8ee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.113b8ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85ff7438a918dc05966",
+    "url": "/static/js/main.c8779c44.chunk.js"
+  },
+  {
+    "revision": "a51e4d6633a514cb252b",
+    "url": "/static/js/runtime-main.792a6471.js"
+  }
+]);
